45096f68472a0c3d5c52f2eea3c4624f9ed8372d
Vishnu <infra-oodles>
2018-07-13 04:19:19 +0530

    update file

--- NewModified.py
+++ NewModified.py
@@ -1,7 +1,26 @@
+#! /usr/bin/python3
 import requests
-import os
-import system
-import commands
-import datetime
-import time
-import web3
+from web3 import Web3,HTTPProvider
+
+def main():
+web3 = Web3(HTTPProvider("http://localhost:8546"))
+geth_latestBlock = web3.eth.blockNumber
+print ('ethereum mainnet latest blocks in Decimal :',a )
+
+etherscan_web3 = requests.get('https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=api_token')
+eth_block = etherscan_web3.json()
+eth_block_latest = eth_block['result']
+
+eth_block_latestInDecimal = int("{}".format(eth_block_latest), 16)
+print ("latest blocks from etherscan.io in Decimal:", eth_block_latestInDecimal)
+
+if geth_latestBlock == eth_block_latestInDecimal:
+  print ("Block syncing is working fine")
+else: 
+  print("Something went wrong!")
+
+
+if __name__ == "__main__":
+print ("Script is running...")
+main()
+print ("Script end")
